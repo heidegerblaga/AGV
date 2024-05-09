b4324b2bad924258ef26580ea29b3f2d9140280c
heidegerblaga <skyrimpluta@gmail.com>
2024-05-09 16:39:27 +0200

    stop

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import socket
+from czujnik import meaure
 
 target_host = "192.168.0.100"
 target_port = 4000
 
 client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+message = bytes([0xFF,0x88,0xFF,0x88])
 
-i = 0
+while meaure().split(";")[2]<="500" and meaure().split(";")[3]<="500" :
+    client.sendto(message,(target_host,target_port))
+
+    data,addr = client.recvfrom(4096)
 
 
-
-message = bytes([0xFF,0x88,0x88,0xFF])
-
-client.sendto(message,(target_host,target_port))
-
+#print(f"odebrano: {data.decode()} z adresu: {addr}")
 
 client.close()
